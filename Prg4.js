f5952c03f6a93652ef0760e7d1fde130ae0127e6
Junaid Ahmed <mehjunaidd@gmail.com>
2025-12-01 10:58:42 +0530

    Create Prg4.js
    Create a Node.js server to read and write JSON objects using files

---
+++ Prg4.js
@@ -0,0 +1,66 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const port = 3000;
+const hostname = '127.0.0.1';
+const DATA_FILE = path.join(__dirname,"product.json");
+
+function readProduct() {
+    if(!fs.existsSync(DATA_FILE)){
+        return [];
+    }
+    const data = fs.readFileSync(DATA_FILE,"utf-8");
+    return JSON.parse(data || "[]");
+}
+
+function writeProduct(products) {
+    fs.writeFileSync(DATA_FILE,JSON.stringify(products,null,2));
+}
+
+const server = http.createServer((req,res) => {
+     const url = req.url;
+     const method = req.method;
+
+     res.setHeader('Content-Type', 'application/json; charset = utf-8');
+
+     //GET products
+     if (url === "/products" && method === "GET"){
+        const products = readProduct();
+        res.end(JSON.stringify(products));
+     }
+     else if (url === "/products" && method === "POST")
+     {
+        let body = "";
+        req.on("data",chunk => {
+            body +=chunk.toString();
+        });
+        req.on("end",() =>{
+            try {
+                const newProduct = JSON.parse(body);
+            if(! newProduct.name || !newProduct.price){
+                res.statusCode = 400;
+                return res.end(JSON.stringify({error: "Name and Price are required"}));
+            }
+            const products = readProduct();
+            products.push(newProduct);
+            writeProduct(products);
+
+            res.statusCode = 201;
+            res.end(JSON.stringify({message:"Product added successfully"}));
+        }
+        catch (error) {
+            res.statusCode = 400;
+            res.end(JSON.stringify({error:"Invalid data"}));
+        }
+        });
+     }
+     else {
+        res.statusCode = 404;
+        res.end(JSON.stringify({error: "Route not found"}));
+     }
+    });
+
+    server.listen (port,hostname,() => {
+        console.group(`Server rinnung on http://${hostname}:${port}/products`);
+    });
+
